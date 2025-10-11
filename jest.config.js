7189bb98fde6f7649ed4a337c94fd1aed161c989
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-11 19:24:06 +0300

    feat:add jest as test dependency

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom', // 👈 important
+};
+
+module.exports = createJestConfig(customJestConfig);
