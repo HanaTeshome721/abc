a9c752f18c697fb9db1c83daec64c653d2c4f7e6
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-19 18:52:08 +0300

    fix:test case add

--- test/Hello.test.jsx
+++ test/Hello.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Hello from '../src/app/components/hello';
 
-describe('Hello component', () => {
+describe('Hello i dont know why time is flying component', () => {
   it('renders the greeting message', () => {
     render(<Hello name="Hana" />);
     expect(screen.getByText('Hello, Hana!')).toBeInTheDocument();
