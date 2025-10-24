14315f6ef83c7834baec704778b8a9b10ebd7460
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-24 21:27:11 +0300

    fix: add test case

--- test/Hello.test.jsx
+++ test/Hello.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Hello from '../src/app/components/hello';
 
 describe('Hello i dont know why time is flying component', () => {
-  it('renders the greeting i gone start next mondey may be message see soon still not started', () => {
+  it('renders the greeting i gone start  i donit even know waht is i going to donext mondey may be message see soon still not started', () => {
     render(<Hello name="Hana" />);
     expect(screen.getByText('Hello, Hana!')).toBeInTheDocument();
   });
